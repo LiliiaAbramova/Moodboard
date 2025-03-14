222a2f697b42f98bb1864f00272d69724c60e45a
liliya.abramova <lili.abramova@gmail.com>
2025-03-14 11:01:14 +0100

    layout changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,13 @@
 "use client";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { SessionProvider } from "next-auth/react";
 import Header from "@/app/components/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
       <SessionProvider>
           <Header />
         {children}
