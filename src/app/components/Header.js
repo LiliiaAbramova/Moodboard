d9e6f0883f7c5c997bf1404945a27c1599cc9be0
LiliiaAbramova <lili.abramova@gmail.com>
2026-07-31 13:22:09 +0200

    Change MVP1 without Auth and AI generation and add manual QA documentation

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <header className="bg-white shadow-md py-4 px-6 flex justify-between items-center">
             <strong className="text-2xl text-gray-900">🎨 Moodboard</strong>
             <nav className="space-x-6">
-                <GoogleAuth />
+                {/*<GoogleAuth />*/}
             </nav>
         </header>
     )
